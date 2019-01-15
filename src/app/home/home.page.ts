07a6736d463e8273f90c758c3ce7ae8f2f02c3cb
Luis Hurtado <luis.hurtado@foodsys.io>
2019-01-15 14:54:26 -0400

    LOGIN

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email = '';
+  password = ''
+   makeLogin() {
+    var headers = new Headers();
+    headers.append("Accept", 'application/json');
+    headers.append('Content-Type', 'application/json' );
+    // const requestOptions = new RequestOptions({ headers: headers });
+
+    // let postData = {
+    //         "email": this.email,
+    //         "password": this.password
+    // }
+
+    this.http.post("url", {"email":this.email, "password":this.password})
+      .subscribe(data => {
+        console.log(data['_body']);
+       }, error => {
+        console.log(error);
+      });
+  }
 
 }
